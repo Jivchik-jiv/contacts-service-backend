64ccc31583fb16f0666e0dd41b9fa8233e099976
Oleksii Zhovchak <jivchik.mail@gmail.com>
2023-07-18 18:39:37 +0300

    user mock fix

--- src/users/users.mock.factories.ts
+++ src/users/users.mock.factories.ts
@@ -6,56 +6,56 @@ import { UpdatePasswordDto } from './dto/update-password.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 export const UserMockFactory = (): ExtendedUserDocument => {
-  return {
-    name: 'name',
-    email: 'email',
-    password: 'password',
-    avatar: 'avatar',
-    cloudAvatarId: 'id',
-    id: 'id',
-    verifyToken: 'verifyToken',
-    token: 'token',
-    verified: true,
-    validatePassword: () => true,
-  };
+    return {
+        name: 'name',
+        email: 'email',
+        password: 'password',
+        avatar: 'avatar',
+        cloudAvatarId: 'id',
+        id: 'id',
+        verifyToken: 'verifyToken',
+        token: 'token',
+        verified: true,
+        validatePassword: () => Promise.resolve(true),
+    };
 };
 
 export const MulterFileMockFactory = (): Express.Multer.File => {
-  const stream = new Readable();
+    const stream = new Readable();
 
-  return {
-    originalname: 'string',
-    mimetype: 'string',
-    path: 'string',
-    buffer: Buffer.from('whatever'),
-    fieldname: 'string',
-    encoding: 'string',
-    size: 55,
-    stream,
-    destination: 'string',
-    filename: 'string',
-  };
+    return {
+        originalname: 'string',
+        mimetype: 'string',
+        path: 'string',
+        buffer: Buffer.from('whatever'),
+        fieldname: 'string',
+        encoding: 'string',
+        size: 55,
+        stream,
+        destination: 'string',
+        filename: 'string',
+    };
 };
 
 export const ExtendedRequestMockFactory = (): ExtendedRequest => {
-  return {
-    user: UserMockFactory(),
-  } as ExtendedRequest;
+    return {
+        user: UserMockFactory(),
+    } as ExtendedRequest;
 };
 
 export const RegisterUserDtoMockFactory = (): RegisterUserDto => {
-  return { name: 'new name', email: 'new email', password: 'new password' };
+    return { name: 'new name', email: 'new email', password: 'new password' };
 };
 
 export const UpdatePasswordDtoMockFactory = (): UpdatePasswordDto => {
-  return {
-    oldPassword: 'oldPassword',
-    newPassword: 'newPasswordL',
-  };
+    return {
+        oldPassword: 'oldPassword',
+        newPassword: 'newPasswordL',
+    };
 };
 
 export const UpdateUserDtoMockFactory = (): UpdateUserDto => {
-  return {
-    name: 'New name',
-  };
+    return {
+        name: 'New name',
+    };
 };
