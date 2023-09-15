9e62100070145c04578448815daa37a34c7bf89b
Oleksii Zhovchak <jivchik.mail@gmail.com>
2023-09-15 12:18:36 +0300

    refactor auth service and controler

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,25 +6,43 @@ import {
   HttpStatus,
   Request,
 } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin.dto';
 import { Public } from '../decorators/public.decorator';
 import { ExtendedRequest } from '../common/extended-requset.interface';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) { }
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService) { }
 
   @Public()
   @HttpCode(HttpStatus.OK)
   @Post('signin')
   async signIn(@Body() signInDto: SignInDto) {
-    return await this.authService.signIn(signInDto);
+    try {
+      const user = await this.usersService.findByEmail(signInDto.email);
+
+      const payload = { id: user.id };
+
+
+      const token = await this.jwtService.signAsync(payload);
+
+      await this.usersService.updateToken(user.id, token);
+
+      return { token, user: { name: user.name, email: user.email, id: user.id, avatar: user.avatar } };
+    } catch (error) {
+      return error;
+    }
+
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Post('signout')
   async signOut(@Request() req: ExtendedRequest) {
-    return this.authService.signOut(req.user.id);
+    await this.usersService.updateToken(req.user.id, null);
+    return true;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import { User, UserSchema } from '../users/schemas/user.schema';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth.guard';
-import { UsersService } from '../users/users.service';
 import { IsCredentialsCorrectConstraint } from './helpers/auth.validators';
 
 @Module({
@@ -22,7 +20,6 @@ import { IsCredentialsCorrectConstraint } from './helpers/auth.validators';
   ],
   controllers: [AuthController],
   providers: [
-    AuthService,
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
